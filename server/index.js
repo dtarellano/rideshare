a4a01239f0c5ae518f77535afcc8982a66209557
David Arelano <dtarellano1@gmail.com>
2017-12-11 20:30:21 -0800

    added test

--- server/index.js
+++ server/index.js
@@ -4,5 +4,9 @@ const app = express();
 
 app.use(express.static(`${__dirname}/../fake`));
 
+router.get('/', (request, response) => {
+  response.status(200).end('bruh');
+});
+
 console.log('listening on port 3000...\n');
 app.listen(3000);
---
+++ test/serverSpec.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+const expect = require('chai').expect;
+const axios = require('axios');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 80;
+
+const baseUrl = `http://localhost:${port}`;
+
+describe('GET /', () => {
+	it('responds with a status code of 200', done => {
+		console.log(port);
+		axios
+			.get(`${baseUrl}/`)
+			.then(response => {
+				expect(response.status).to.equal(200);
+				done();
+			})
+			.catch(err => {
+				console.log('There was an error requesting / from the server', err);
+			});
+	});
+});
+
+/* eslint-enable */
+
